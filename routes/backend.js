f47b9f55a6831bddf8738e7a3a899a6c7f5fb9d0
RaistlinTAO <DemonVK@Gmail.com>
2019-03-25 15:18:45 +1100

    Switch to lightweight framework

--- routes/backend.js
+++ routes/backend.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
+/* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index');
+    res.send('backend');
 });
 
 module.exports = router;
