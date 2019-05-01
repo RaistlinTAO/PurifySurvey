b8fb4017c78195d99dc87383000e8b15a1bdd06b
RaistlinTAO <DemonVK@Gmail.com>
2019-05-01 20:22:00 +1000

    Finlisation

--- routes/index.js
+++ routes/index.js
@@ -13,4 +13,12 @@ router.get('/login', function(req, res) {
   res.sendfile('public/login.html');
 });
 
+router.get('/survey_create', function(req, res) {
+  res.sendfile('public/survey_create.html');
+});
+
+router.post('survey_submit', function(req, res) {
+  res.sendfile('public/login.html');
+});
+
 module.exports = router;
